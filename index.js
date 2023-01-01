2eba3a57bee2b1504b0f3a432dc1528f31d59b52
OsamahKhOdeh <121430755+OsamahKhOdeh@users.noreply.github.com>
2023-01-01 16:11:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("HEEEEEEEELO") )
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)); )
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
 
-app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
+
 
 module.exports = app;
 
